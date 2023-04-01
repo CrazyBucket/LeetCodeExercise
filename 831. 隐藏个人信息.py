d3cfee2eba6cde8d991cf8f7c1d4aecedc892415
CrazyBucket <1475743203@qq.com>
2023-04-01 17:11:07 +0800

    831

---
+++ 831. 隐藏个人信息.py
@@ -0,0 +1,31 @@
+import re
+
+
+class Solution:
+    def number(self, s: str) -> str:
+        s = re.findall(r'\d+', s)
+        s = ''.join(s)
+        p = s[0: len(s) - 10]
+        a = s[len(s) - 10: len(s)]
+        if len(p) == 0:
+            return "***-***-" + a[len(a) - 4:len(a)]
+        return '+' + '*' * len(p) + "-***-***-" + a[len(a) - 4:len(a)]
+
+    def email(self, s: str) -> str:
+        p = s.split("@")[0].lower()
+        a = s.split("@")[1].lower()
+        # .split可以以"字符"分割成字符串数组
+        # print(str.upper())   把所有字符中的小写字母转换成大写字母
+        # print(str.lower())   把所有字符中的大写字母转换成小写字母
+        # print(str.capitalize())   把第一个字母转化为大写字母，其余小写
+        # print(str.title())   把每个单词的第一个字母转化为大写，其余小写
+        return p[0] + '*****' + p[len(p) - 1] + "@" + a
+
+    def maskPII(self, s: str) -> str:
+        if s.count("@") == 1:
+            return self.email(s)
+        return self.number(s)
+
+
+solution = Solution()
+print(solution.maskPII("86-(10)12345678"))
