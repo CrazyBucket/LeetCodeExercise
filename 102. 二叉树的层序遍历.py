437a4181303f87a29becc57599c08821c004ff36
CrazyBucket <1475743203@qq.com>
2023-04-15 23:24:35 +0800

    102 BFS

---
+++ 102. 二叉树的层序遍历.py
@@ -0,0 +1,43 @@
+from typing import List, Optional
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        result = []
+        if not root:
+            return result
+
+        queue = deque()
+        queue.append(root)
+
+        while queue:
+            level = []
+            level_size = len(queue)
+            for i in range(level_size):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(level)
+
+        return result
+
+
+root = TreeNode(3)
+root.left = TreeNode(9)
+root.right = TreeNode(20)
+root.right.left = TreeNode(15)
+root.right.right = TreeNode(7)
+
+solution = Solution()
+print(solution.levelOrder(root))
