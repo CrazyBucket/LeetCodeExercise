625f80cca5a7acdbd4b71002738d1574ca68516f
CrazyBucket <1475743203@qq.com>
2023-09-24 22:56:55 +0800

    27

---
+++ 27. 移除元素.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === val) {
+      nums.splice(i, 1);
+      i--;
+    }
+  }
+};
+
+let nums = [3, 2, 2, 3];
+let val = 3;
+removeElement(nums, val);
