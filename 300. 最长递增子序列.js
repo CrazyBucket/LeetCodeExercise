a790032418a8f1ddebeba0e12d8096497d42b5c0
CrazyBucket <1475743203@qq.com>
2023-10-03 23:09:46 +0800

    300

---
+++ 300. 最长递增子序列.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var lengthOfLIS = function (nums) {
+  const dp = new Array(nums.length).fill(1);
+  for (let i = 1; i < nums.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (nums[i] > nums[j]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+  }
+  let maxLength = 1;
+  for (let i = 0; i < dp.length; i++) {
+    maxLength = Math.max(maxLength, dp[i]);
+  }
+  return maxLength;
+};
+let nums = [7, 7, 7, 7];
+console.log(lengthOfLIS(nums));
